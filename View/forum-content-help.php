1b4766b2c515b7b13d2d7fa80cd2fdcacdd1b26f
matej2000 <matejvatovec22@gmail.com>
2021-07-25 17:41:17 +0200

    Bolj pregledno.

--- View/forum-content-help.php
+++ View/forum-content-help.php
@@ -4,19 +4,19 @@ class contentHelp{
     public static function publicPost($forum, $author, $foruml, $forumlc){
 
 
-        include("forum-content-public.php");
+        include("Content-visibility/forum-content-public.php");
     }
 
     public static function privatePost($forum, $author, $foruml, $forumlc){
-        include("forum-content-private.php");
+        include("Content-visibility/forum-content-private.php");
     }
 
     public static function publicPostComment($forum, $author, $foruml, $forumlc){
-        include("forum-content-public-comment.php");
+        include("Content-visibility/forum-content-public-comment.php");
     }
 
     public static function privatePostComment($forum, $author, $foruml, $forumlc){
-        include("forum-content-public-comment.php");
+        include("Content-visibility/forum-content-public-comment.php");
     }
 
 }
